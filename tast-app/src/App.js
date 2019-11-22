a8baed91d7ff98f6e52cff2abe4b3331d5e3cab9
mohammed imtiyaz <mohammed.imtiyaz@signify.com>
2019-11-22 12:43:18 +0530

    removes unwanted files

--- tast-app/src/App.js
+++ tast-app/src/App.js
@@ -1,14 +1,8 @@
-import React, { Component } from 'react';
-import './App.css';
-import Button from './Button';
+import React, { Component } from "react";
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <Button />
-      </div>
-    );
+    return <div>App</div>;
   }
 }
 
